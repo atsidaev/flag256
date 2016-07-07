69e6dacecc1708b9ce40fc968ab05567eaa514e5
Alexander Tsidaev <a.tsidaev@gmail.com>
2016-07-07 17:09:49 +0500

    Changeable direction

--- main.asm
+++ main.asm
@@ -12,7 +12,6 @@ COLOR_K EQU 0 ; black
 	LD SP, #FFFF
 	XOR A
 	OUT (#FE), A
-
 	ld hl, 16384 + 6144
 ;BLACK/WHITE
 	LD A, COLOR_K
@@ -39,6 +38,7 @@ COLOR_K EQU 0 ; black
 	LD (WAVE_COLOR_2), A
 	CALL WAVE
 	
+	halt
 	halt
 
 	CALL SCROLL_WAVE
@@ -90,13 +90,45 @@ PUT_COLOR:
 	
 	RET
 
+CHANGE_SCROLL_DIRECTION:
+	LD A, (SCROLL_DIRECTION)
+	XOR #FF
+	LD (SCROLL_DIRECTION), A
+	RET
+
 SCROLL_WAVE:
 	; scrolling flag tail
+	
+	; first check if we need to change scroll direction
+	; next byte from ROM
+	INC IY
+	LD A, IYH
+	AND #7F
+	LD IYH, A
+	LD A, (IY)
+	AND #03
+	CP 2
+	CALL Z, CHANGE_SCROLL_DIRECTION
+	LD A, (SCROLL_DIRECTION)
+	AND 1
+	JP Z, SCROLL_WAVE_LEFT
+SCROLL_WAVE_RIGHT:
+	LD HL, WAVE_DATA + 62
+	LD DE, WAVE_DATA + 63
+	LD A, (DE)
+	LD BC, 63
+	LDDR
+
+	JR SCROLL_WAVE_END
+SCROLL_WAVE_LEFT:
 	LD HL, WAVE_DATA + 1
 	LD DE, WAVE_DATA
 	LD A, (DE)
 	LD BC, 63
 	LDIR
+
+SCROLL_WAVE_END:
+	; restoring first affected byte of scrolled array
 	LD (DE), A
 	
 	; calculating flag head
@@ -134,6 +166,7 @@ SET_HEAD_POS;
 
 	JP INTERPOLATE_HEAD
 
+SCROLL_DIRECTION: DB 0
 FLAG_DATA:
 	DB #F0
 	DB 0
